2959a9fd536f16328e62786c4fedf556002c0077
David Van Der Beek <earlynovrock@gmail.com>
2012-06-27 14:56:18 -0500

    add back friendly_id

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,6 @@
 class Site < ActiveRecord::Base
   extend FriendlyId
-  #friendly_id :subdomain, use: :slugged
+  friendly_id :subdomain, use: :slugged
   belongs_to :user
   has_many :posts
   validates :subdomain, :presence => true, :uniqueness => true
