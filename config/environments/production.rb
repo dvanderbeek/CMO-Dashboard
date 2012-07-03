e883877dbbc179249ffb5fc0e118444c38ab19ca
David Van Der Beek <earlynovrock@gmail.com>
2012-07-03 17:53:06 -0500

    precompile jquery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ CMODashboard::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['site.css', 'design.css', 'jsp.css', 'jsp.js', 'designs.js']
+  config.assets.precompile += ['jquery.js', 'site.css', 'design.css', 'jsp.css', 'jsp.js', 'designs.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
